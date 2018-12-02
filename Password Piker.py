5e6ad09cf8eca485ed2050e66358a5481b7c9e54
Juanita563 <jlanderson563@gmail.com>
2018-12-02 12:37:37 -0500

    Initial commit

---
+++ Password Piker.py
@@ -0,0 +1,19 @@
+# The Program randomly selects the passwords four parts, puts them together, and displays the.
+# Passwords in the shell window. If you want another Password, it repeats those steps again.
+# If not the program ends
+
+import random
+import string
+
+adjectives = ['sleepy', 'slow', 'smelly', 'wet', 'fat', 'red', 'orange'
+              'yellow', 'green', 'blue', 'purple', 'fluffy']
+
+noun = ['apple', 'dinisour', 'ball', 'toaster', 'goat',
+        'dragon', 'hammer', 'duck', 'panda']
+
+print('Welcome to Password Picker!')
+
+
+
+
+print('Welcome to Password Picker!')
